6a5e431c7c757c84bfc44c11d1fae333f77b7980
Sharon Mwenda <smkathambi@gmail.com>
2022-06-07 18:33:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity){
+    if(activity === undefined){
+        return "This Saturday, I want to roller-skate!"
+    }
+    else{
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+saturdayFun();
+
+const mondayWork = function(place) {
+    if(place === undefined){
+        return "This Monday, I will go to the office."
+    }
+    else{
+        return `This Monday, I will ${place}.`
+    }
+}
+
+function wrapAdjective(wrap){
+    const star = function(adjective) {
+        return `You are ${wrap}${adjective}${wrap}!`;
+    }
+    return star;
+}
+   
