0706cbd6e12dc951e720c40e525f4bd733b009be
Sharon Mwenda <smkathambi@gmail.com>
2022-06-07 18:32:33 +0300

    commit tree 756a8c5352c61dc7174b7fe7638021671a660a3f

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(activity){
+    if(activity === undefined){
+        return "This Saturday, I want to roller-skate!"
+    }
+    else{
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+saturdayFun();
+
+const mondayWork = function(place) {
+    if(place === undefined){
+        return "This Monday, I will go to the office."
+    }
+    else{
+        return `This Monday, I will ${place}.`
+    }
+}
+
+function wrapAdjective(wrap){
+    const star = function(adjective) {
+        return `You are ${wrap}${adjective}${wrap}!`;
+    }
+    return star;
+}
+   /* return `You are ${wrap} a hard worker ${wrap}`;
+}
+    const line = wrapAdjective("||")
+    return `You are ${wrap} a dedicated programmer ${wrap}`;
+    }
+}
+wrapAdjective("*"); */
